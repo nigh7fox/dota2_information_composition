1247774d646654bca823e7762c3256bdea37f4f7
Joshua S. Tromp <joshwow91@gmail.com>
2016-04-27 18:40:52 +0200

    How to use with the new DotaData class

--- Main.py
+++ Main.py
@@ -1,16 +1,7 @@
 import Dota2 as d2
-import urllib.request as url
 
-# Catch HTMLErorr.
-def display_data():
-    try:
-        d2.display_information("19838652")
-        d2.display_dota2_news("19838652")
-    except url.HTTPError:
-        print("Error has occurred while downloading data xml data.")
-    else:
-        None
+#   d2.display_dota2_news("19838652")
+#   twitter.tweet()
 
-display_data()
-
-
+emp = d2.DotaData("19838652")
+emp.display_all_data()
