727f4553539729dfc5b2df9a77172dac35477e85
Joshua S. Tromp <joshwow91@gmail.com>
2016-04-29 14:37:05 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
 import Dota2 as d2
+import urllib.request as url
+import time
 
-#   d2.display_dota2_news("19838652")
-#   twitter.tweet()
-
-emp = d2.DotaData("19838652")
-emp.display_all_data()
+emp = d2.DotaData("19838652") # YOUR STEAM ID / SOME FUNCTIONS WORK WITH COMMUNITY URL
+error = True
+reconnect_tries = 0
+sleep_timer = 5
+games_list = []
+while error is True:
+    try:
+        for i in emp.list_results(5, 0): # EXAMPLE FUNCTION
+            print(i)
+        error = False
+    except url.HTTPError: # RECONNECT IF HTTPError is thrown.
+        reconnect_tries += 1
+        print("HTTPError has occurred while downloading data xml data." +
+              "\nTrying to reconnect.." + "(" + str(reconnect_tries) + ")"  "\n")
+        time.sleep(sleep_timer)
